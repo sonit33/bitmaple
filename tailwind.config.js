1fb765d366accca8a2df0b8859f9ee7f35c5d45c
Sonit <sonitbuilds+git@gmail.com>
2023-06-09 16:04:47 -0700

    testimonial

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
         "fb-blue": "#445793",
         brand: "#7EC044",
         black: { 500: "#3a3a3a", 600: "#1e1e1e" },
-        white: { 400: "#cacaca", 500: "#eaeaea", 600: "#fefefe" },
+        white: { 300: "#acacac", 400: "#cacaca", 500: "#eaeaea", 600: "#fefefe" },
         info: "",
         warning: "",
         success: "",
