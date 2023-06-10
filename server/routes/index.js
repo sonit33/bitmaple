5f0247f8ac8e042b855e2e45369a4f4b3977c046
Sonit <sonitbuilds+git@gmail.com>
2023-06-09 17:28:35 -0700

    consulting route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "BitMaple" });
+  res.render("index", { title: "Welcome to BitMaple" });
+});
+
+router.get("/consulting", function (req, res, next) {
+  res.render("consulting/index", { title: "BitMaple Consulting" });
 });
 
 module.exports = router;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       colors: {
         "fb-blue": "#445793",
         brand: "#7EC044",
-        black: { 500: "#3a3a3a", 600: "#1e1e1e" },
+        black: { 400: "#5a5a5a", 500: "#3a3a3a", 600: "#1e1e1e" },
         white: { 300: "#acacac", 400: "#cacaca", 500: "#eaeaea", 600: "#fefefe" },
         info: "",
         warning: "",
